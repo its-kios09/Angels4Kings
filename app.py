981cfaa34c438b00f7d4ca180769e3bdcf726050
blackcode-creator <legacyitsolution@gmail.com>
2023-08-07 23:00:17 +0300

    integrated flask and firebase

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from views import views
+# Initializing flask app
+
+app = Flask(__name__)
+# Registering blueprint views
+app.register_blueprint(views,url_prefix='/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+views = Blueprint(__name__,"views")
+
+# Defining our routes
+# home page route
+@views.route('/')
+def index():
+    try:
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
+
+@views.route('/auth-view')
+def auth_view():
+    try:
+        return render_template('login.html')
+    except TemplateNotFound:
+        abort(404)
